cdc20a3fd87b79c2d228553bbbb445da88af36c0
coder9t99 <coder9t99@gmail.com>
2016-05-05 16:29:27 +1000

    Initial Commit

---
+++ Card Match/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Card Match
+//
+//  Created by Quentin Lin on 5/05/2016.
+//  Copyright © 2016 Accedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Card Match/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Card Match
+//
+//  Created by Quentin Lin on 5/05/2016.
+//  Copyright © 2016 Accedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
