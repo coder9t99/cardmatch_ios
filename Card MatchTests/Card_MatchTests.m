16a6b3f49bad85678eca0dd44401bd6136f3edb5
coder9t99 <coder9t99@gmail.com>
2016-05-08 19:53:03 +1000

    added unit tests

--- Card MatchTests/Card_MatchTests.m
+++ Card MatchTests/Card_MatchTests.m
@@ -7,33 +7,112 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "CardMatch.h"
 
 @interface Card_MatchTests : XCTestCase
-
+@property (nonatomic, strong) CardMatch *cardMatch;
 @end
 
+#pragma mark - helper classes definitions
+@interface CardMatchDelegateTester : NSObject<CardMatchDelegate>
+@property BOOL triggered;
+@end
+
+
+#pragma mark - test body
 @implementation Card_MatchTests
 
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.cardMatch = CardMatch.new;
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    self.cardMatch = nil;
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testRestartGameWillShuffleCardData {
+    NSArray<NSNumber*> *cardDataBeforeRestart = [self.cardMatch.cardData copy];
+    [self.cardMatch start];
+    NSArray<NSNumber*> *cardDataAfterRestart = self.cardMatch.cardData;
+
+    BOOL isCardDataSame = YES;
+    for (NSUInteger i = 0; i < cardDataBeforeRestart.count; i++) {
+        NSNumber *cardNumber1 = cardDataBeforeRestart[i];
+        NSNumber *cardNumber2 = cardDataAfterRestart[i];
+        if (![cardNumber1 isEqualToNumber:cardNumber2]) {
+            isCardDataSame = NO;
+            break;
+        }
+    }
+
+    XCTAssertFalse(isCardDataSame, @"card data before and after `start` should be different.");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testCorrectMatchWillAddTwoPoints {
+    NSNumber *targetNumber = @(1);
+    NSMutableArray<NSNumber*> *positions = [NSMutableArray arrayWithCapacity:2];
+    for (NSUInteger i = 0; i < self.cardMatch.totalCardCount; i++) {
+        if ([targetNumber isEqualToNumber:self.cardMatch.cardData[i]]) {
+            [positions addObject:@(i)];
+        }
+    }
+    [self.cardMatch match:positions[0].unsignedIntegerValue with:positions[1].unsignedIntegerValue];
+
+    XCTAssertEqual(self.cardMatch.currentScore, 2, @"correct match should result +2 points");
 }
 
+- (void)testIncorrectMatchWillReduceONePoint {
+    NSUInteger pos1 = 0;
+    NSUInteger pos2 = 0;
+    NSNumber *targetNumber = self.cardMatch.cardData[pos1];
+    for (NSUInteger i = 1; i < self.cardMatch.totalCardCount; i++) {
+        if (![targetNumber isEqualToNumber:self.cardMatch.cardData[i]]) {
+            pos2 = i;
+        }
+    }
+    [self.cardMatch match:pos1 with:pos2];
+
+    XCTAssertEqual(self.cardMatch.currentScore, -1, @"correct match should result -1 points");
+}
+
+- (void)testWhenAllCardMatchedWillTriggerGameOver {
+    CardMatchDelegateTester *delegateTester = CardMatchDelegateTester.new;
+    self.cardMatch.delegate = delegateTester;
+
+    // match all
+    for (NSUInteger target = 0; target < self.cardMatch.totalCardCount / 2; target++) {
+        NSNumber *targetNumber = @(target+1);
+        NSMutableArray<NSNumber*> *positions = [NSMutableArray arrayWithCapacity:2];
+        for (NSUInteger i = 0; i < self.cardMatch.totalCardCount; i++) {
+            if ([targetNumber isEqualToNumber:self.cardMatch.cardData[i]]) {
+                [positions addObject:@(i)];
+            }
+        }
+        [self.cardMatch match:positions[0].unsignedIntegerValue with:positions[1].unsignedIntegerValue];
+    }
+
+    XCTAssertTrue(delegateTester.triggered, @"when all card matched `gameOver:` should be triggered.");
+}
+
+@end
+
+#pragma mark - helper classes implementations
+@implementation CardMatchDelegateTester
+
+-(instancetype)init {
+    self = super.init;
+    if (self) {
+        self.triggered = NO;
+    }
+    return self;
+}
+
+-(void)gameOver:(NSInteger)score
+{
+    self.triggered = YES;
+}
 @end
